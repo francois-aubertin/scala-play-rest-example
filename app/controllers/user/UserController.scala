8084f1903ddf6ad65fb595047a73e07987a605b3
Francois Aubertin <francois.aubertin@gmail.com>
2013-06-04 22:59:27 +0100

    Adding resources.

--- app/controllers/user/UserController.scala
+++ app/controllers/user/UserController.scala
@@ -13,6 +13,15 @@ trait UserController extends Controller {
     implicit val userReads = (__ \ "email").read[String]
                                            .map(resource => UserResource(resource))
     
+    implicit val userWrites = new Writes[User] {
+        override def writes(user: User): JsValue = {
+            Json.obj(
+                "id" -> user.id,
+                "email" -> user.email
+            )
+        }
+    }
+                                           
     def createUser = Action(parse.json) {request =>
         unmarshalUserResource(request, (resource: UserResource) => {
             val user = User(Option.empty,
@@ -29,6 +38,20 @@ trait UserController extends Controller {
             userService.updateUser(user)
             NoContent
         })
+    }
+    
+    def findUserById(id: Long) = Action {
+        val user = userService.tryFindById(id)
+        if (user.isDefined) {
+            Ok(Json.toJson(user))
+        } else {
+            NotFound
+        }
+    }
+    
+    def deleteUser(id: Long) = Action {
+        userService.delete(id)
+        NoContent
     }
     
     private def unmarshalUserResource(request: Request[JsValue],
